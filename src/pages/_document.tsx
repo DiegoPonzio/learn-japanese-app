ec37cc47f61c4c24d629da23ba10402f6478e166
DiegoPonzio <druizp2000@alumno.ipn.mx>
2023-08-16 18:08:01 -0600

    Add google adsense

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head
+      >
+          <script async
+                  src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2905575800221472"
+                  crossOrigin="anonymous"></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
